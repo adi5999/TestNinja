2933e5650639de88766ca4a5b6e3146a56fbb8c8
costrategix <costrategix@DESKTOP-FKI33GR>
2022-09-30 14:13:20 +0530

    Add project files.

---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+using TestNinja.UnitTests;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //Arrange
+            var reservation=new Reservation();
+            //act
+            var result=reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void CanBeCancelledBy_SameUserCancellingTheReservation_ReturnTrue()
+        {
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+            //aarrange
+            var result= reservation.CanBeCancelledBy(user);   
+            //act
+
+            Assert.IsTrue(result);
+            //assert
+        }
+        [TestMethod]
+        public void CanBeCancelledBy_AnotherUsercancellingReservation_RetutnsFalse()
+        {
+            var reservation = new Reservation { MadeBy = new User() };
+
+            var result = reservation.CanBeCancelledBy(new User());
+            Assert.IsFalse(result);
+        }
+    }
+}
